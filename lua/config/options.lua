db4f921bebef037d159617e94cbfbbfab5e53d81
Justin Angel <justinangel86@gmail.com>
2026-06-10 13:58:23 -0400

    updates

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,3 +1,40 @@
+-- languages that are injected into gtemp
+-- files based on extension.
+local gtemp_langs = {
+  yml = "yaml",
+  yaml = "yaml",
+  json = "json",
+  html = "html",
+  bash = "bash",
+  sh = "bash",
+  env = "bash",
+}
+
+-- get the parser for gtemp/gotmp file types.
+--
+-- format file names like: stuff.yml.gotmpl
+--
+-- the injected file type is extracted from
+-- the second field of the file name.
+--
+-- see gtemp_langs for supported values.
+--
+-- see plugins/treesitter.lua for information
+-- on where things are stored.
+function get_gotmpl_parser(path, bufnr)
+  local full = vim.fn.fnamemodify(path, ":t")
+  local ftype = full:match("^.+%.(.+)%.")
+  local lang = ftype and gtemp_langs[ftype]
+
+  local ft = "gotmpl"
+  if lang then
+    vim.b[bufnr].gtemp_injection_language = lang
+    ft = gtemp_langs[lang] .. "_" .. "gotmpl"
+  end
+
+  return ft
+end
+
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
@@ -14,10 +51,13 @@ vim.filetype.add({
     -- had to add a treesitter injection to enable html highlighting
     -- within go template files
     -- see: queries/gohtml/injections.scm
-    gohtml = "gotmpl",
+    gohtml = "html_gotmpl",
+    gtemp = get_gotmpl_parser,
     cue = "cue",
   },
 })
+
+vim.opt.mouse = "" -- disable mouse
 
 -- ENABLE PER-PROJECT RC/SCRIPTS
 vim.opt.exrc = true -- allow per-project rc files (.nvim.lua)
--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,23 +1,91 @@
 return {
   {
-    "nvim-treesitter",
-    opts = {
-      ensure_installed = {
-        -- "cue",
-        "lua",
-        "yaml",
-        "bash",
-        -- golang
-        "go",
+    "nvim-treesitter/nvim-treesitter",
+    lazy = false,
+    build = ":TSUpdate",
+    config = function(_, opts)
+      require("nvim-treesitter").setup(opts)
+
+      vim.api.nvim_create_autocmd("User", {
+        pattern = "TSUpdate",
+        callback = function()
+          local parsers = require("nvim-treesitter.parsers")
+          local repo = vim.fn.expand("~/.config/nvim/tree-sitter-go-template-gtemp")
+
+          parsers.html_gotmpl = {
+            -- tier = 1,
+            install_info = {
+              -- url = "",
+              -- revision = "",
+              path = repo,
+              location = "dialects/html_gotmpl",
+              generate = false,
+              queries = "queries/html_gotmpl",
+            },
+          }
+          parsers.yaml_gotmpl = {
+            -- tier = 1,
+            install_info = {
+              -- url = "",
+              -- revision = "",
+              path = repo,
+              location = "dialects/yaml_gotmpl",
+              generate = true,
+              queries = "queries/yaml_gotmpl",
+            },
+          }
+          parsers.json_gotmpl = {
+            -- tier = 1,
+            install_info = {
+              -- url = "",
+              -- revision = "",
+              path = repo,
+              location = "dialects/json_gotmpl",
+              generate = true,
+              queries = "queries/json_gotmpl",
+            },
+          }
+          parsers.bash_gotmpl = {
+            -- tier = 1,
+            install_info = {
+              -- url = "",
+              -- revision = "",
+              path = repo,
+              location = "dialects/bash_gotmpl",
+              generate = true,
+              queries = "queries/bash_gotmpl",
+            },
+          }
+        end,
+      })
+    end,
+
+    opts = function(_, opts)
+      opts.local_parsers = opts.local_parsers or {}
+      opts.ensure_installed = opts.ensure_installed
+        or {
+          -- "cue",
+          "lua",
+          "yaml",
+          "bash",
+          -- golang
+          "go",
+          "gotmpl",
+          "gomod",
+          -- python
+          "python",
+          -- webdev
+          "html",
+          "css",
+          "javascript",
+        }
+
+      vim.list_extend(opts.ensure_installed, {
         "gotmpl",
-        "gomod",
-        -- python
-        "python",
-        -- webdev
-        "html",
-        "css",
-        "javascript",
-      },
-    },
+        "html_gotmpl",
+        "yaml_gotmpl",
+        "json_gotmpl",
+      })
+    end,
   },
 }
